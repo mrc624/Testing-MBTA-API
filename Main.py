bde84898316f7b4e24e0840b1811dd9a23f75a66
Matthew Cloutier <mrcloutier624@gmail.com>
2025-05-19 16:14:27 -0400

    getting api response

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv("key.env")
@@ -6,3 +7,16 @@ load_dotenv("key.env")
 key = str(os.getenv('MBTA_API_KEY'))
 
 print("\n Key: " + key + "\n")
+
+url = "https://api-v3.mbta.com/vehicles"
+
+params = { }
+
+headers = {
+    "x-api-header":key
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+print(response.status_code)
+print(response.json())
