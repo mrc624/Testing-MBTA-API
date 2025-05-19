1b3a85c52661e087d06c14ab98f3b8e619611048
Matthew Cloutier <mrcloutier624@gmail.com>
2025-05-19 17:03:50 -0400

    playing around with some different stats

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -8,7 +9,9 @@ key = str(os.getenv('MBTA_API_KEY'))
 
 print("\n Key: " + key + "\n")
 
-url = "https://api-v3.mbta.com/vehicles"
+url = "https://api-v3.mbta.com"
+vehicles_url = url + "/vehicles"
+routes_url = url + "/routes"
 
 params = { }
 
@@ -16,7 +19,62 @@ headers = {
     "x-api-header":key
 }
 
-response = requests.get(url, params=params, headers=headers)
+response = requests.get(routes_url, params=params, headers=headers)
+print("Routes Response: " + str(response.status_code))
+routes = response.json()
 
-print(response.status_code)
-print(response.json())
+with open('routes.json', 'w') as fp:
+    json.dump(routes, fp, indent=4)
+
+
+response = requests.get(vehicles_url, params=params, headers=headers)
+print("Vehicles Response: " + str(response.status_code))
+vehicles = response.json()
+
+with open('vehicles.json', 'w') as fp:
+    json.dump(vehicles, fp, indent=4)
+
+route_id_name = { }
+
+for route in routes["data"]:
+    route_id = route["id"]
+    route_name = route["attributes"]["long_name"]
+    route_id_name[route_id] = route_name
+
+with open('routes_id_name.json', 'w') as fp:
+    json.dump(route_id_name, fp, indent=4)
+
+vehicles_on_route_count = { }
+
+for route in routes["data"]:
+    vehicles_on_route_count[route["id"]] = 0
+
+for vehicle in vehicles["data"]:
+    route_id = vehicle["relationships"]["route"]["data"]["id"]
+    if route_id in vehicles_on_route_count:
+        curr_count = vehicles_on_route_count[route_id]
+        new_count = curr_count + 1
+        vehicles_on_route_count[route_id] = new_count
+    else:
+        vehicles_on_route_count[route_id] = 1
+
+with open('vehicles_on_route_count.json', 'w') as fp:
+    json.dump(vehicles_on_route_count, fp, indent=4)
+
+vehicles_on_route_count_long = { }
+
+for id in route_id_name:
+    count = vehicles_on_route_count[id]
+    vehicles_on_route_count_long[route_id_name[id]] = count
+    print("run")
+
+with open('vehicles_on_route_count_long.json', 'w') as fp:
+    json.dump(vehicles_on_route_count_long, fp, indent=4)
+
+
+
+
+
+
+#with open('result.json', 'w') as fp:
+#    json.dump(data, fp, indent=4)
