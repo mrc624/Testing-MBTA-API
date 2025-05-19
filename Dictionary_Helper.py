dfd1fa19ed1c80b6ac68dbbaea42ef1b9e1c38c3
Matthew Cloutier <mrcloutier624@gmail.com>
2025-05-19 19:35:58 -0400

    added failed request checks

--- Dictionary_Helper.py
+++ Dictionary_Helper.py
@@ -10,6 +10,8 @@ key = str(os.getenv('MBTA_API_KEY'))
 DEBUG = True
 DEBUG_FOLDER = "Debug\\"
 
+GOOD_RESPONSE = 200
+
 url = "https://api-v3.mbta.com"
 alerts_url = url + "/alerts"
 facilities_url = url + "/facilities"
@@ -36,73 +38,88 @@ def Get_Routes(params = default_params, headers = default_headers):
     routes = response.json()
     if DEBUG:
         print("Routes Response: " + str(response.status_code))
-        with open(DEBUG_FOLDER + 'routes.json', 'w') as fp:
-            json.dump(routes, fp, indent=4)
-    return routes
+        if (response.status_code is GOOD_RESPONSE):
+            with open(DEBUG_FOLDER + 'routes.json', 'w') as fp:
+                json.dump(routes, fp, indent=4)
+    if (response.status_code is GOOD_RESPONSE):
+        return routes
+    else:
+        return None
 
 def Get_Vehicles(params = default_params, headers = default_headers):
     response = requests.get(vehicles_url, params=params, headers=headers)
     vehicles = response.json()
     if DEBUG:
         print("Vehicles Response: " + str(response.status_code))
-        with open(DEBUG_FOLDER + 'vehicles.json', 'w') as fp:
-            json.dump(vehicles, fp, indent=4)
-    return vehicles
+        if (response.status_code is GOOD_RESPONSE):
+            with open(DEBUG_FOLDER + 'vehicles.json', 'w') as fp:
+                json.dump(vehicles, fp, indent=4)
+    if (response.status_code is GOOD_RESPONSE):
+        return vehicles
+    else:
+        return None
 
 def Get_ID_to_Names(params = default_params, headers = default_headers):
     routes = Get_Routes(params=params, headers=headers)
-    ID_to_Names = { }
-    for route in routes["data"]:
-        route_id = route["id"]
-        route_short_name = route["attributes"]["short_name"]
-        route_long_name = route["attributes"]["long_name"]
-        ID_to_Names[route_id] = {
-            "short_name": route_short_name,
-            "long_name": route_long_name
-        }
-    if DEBUG:
-        with open(DEBUG_FOLDER + 'routes_id_name.json', 'w') as fp:
-            json.dump(ID_to_Names, fp, indent=4)
-    return ID_to_Names
+    if routes is not None:
+        ID_to_Names = { }
+        for route in routes["data"]:
+            route_id = route["id"]
+            route_short_name = route["attributes"]["short_name"]
+            route_long_name = route["attributes"]["long_name"]
+            ID_to_Names[route_id] = {
+                "short_name": route_short_name,
+                "long_name": route_long_name
+            }
+        if DEBUG:
+            with open(DEBUG_FOLDER + 'routes_id_name.json', 'w') as fp:
+                json.dump(ID_to_Names, fp, indent=4)
+        return ID_to_Names
 
 def Get_Vehicles_on_Routes_Count(params = default_params, headers = default_headers):
     routes = Get_Routes(params=params, headers=headers)
     vehicles = Get_Vehicles(params=params, headers=headers)
-    vehicles_on_route_count = { }
-    for route in routes["data"]:
-        vehicles_on_route_count[route["id"]] = 0
+    if routes is not None and vehicles is not None:
+        vehicles_on_route_count = { }
+        for route in routes["data"]:
+            vehicles_on_route_count[route["id"]] = 0
 
-    for vehicle in vehicles["data"]:
-        route_id = vehicle["relationships"]["route"]["data"]["id"]
-        if route_id in vehicles_on_route_count:
-            curr_count = vehicles_on_route_count[route_id]
-            new_count = curr_count + 1
-            vehicles_on_route_count[route_id] = new_count
-        else:
-            vehicles_on_route_count[route_id] = 1
-    if DEBUG:
-        with open(DEBUG_FOLDER + 'vehicles_on_route_count.json', 'w') as fp:
-            json.dump(vehicles_on_route_count, fp, indent=4)
-    return vehicles_on_route_count
+        for vehicle in vehicles["data"]:
+            route_id = vehicle["relationships"]["route"]["data"]["id"]
+            if route_id in vehicles_on_route_count:
+                curr_count = vehicles_on_route_count[route_id]
+                new_count = curr_count + 1
+                vehicles_on_route_count[route_id] = new_count
+            else:
+                vehicles_on_route_count[route_id] = 1
+        if DEBUG:
+            with open(DEBUG_FOLDER + 'vehicles_on_route_count.json', 'w') as fp:
+                json.dump(vehicles_on_route_count, fp, indent=4)
+        return vehicles_on_route_count
 
 def Get_Lines(params = default_params, headers = default_headers):
     response = requests.get(lines_url, params=params, headers=headers)
     lines = response.json()
     if DEBUG:
         print("Lines Response: " + str(response.status_code))
-        with open(DEBUG_FOLDER + 'lines.json', 'w') as fp:
-            json.dump(lines, fp, indent=4)
-    return lines
+        if (response.status_code is GOOD_RESPONSE):
+            with open(DEBUG_FOLDER + 'lines.json', 'w') as fp:
+                json.dump(lines, fp, indent=4)
+    if (response.status_code is GOOD_RESPONSE):
+        return lines
+    else:
+        return None
 
 def Get_Line_Names(params = default_params, headers = default_headers):
     lines = Get_Lines(params=params, headers=headers)
-    line_names = { }
-    for line in lines["data"]:
-        line_names[line["id"]] = {
-            "short_name": line["attributes"]["short_name"],
-            "long_name": line["attributes"]["long_name"]
-        }
-    if DEBUG:
-        with open(DEBUG_FOLDER + 'line_names.json', 'w') as fp:
-            json.dump(line_names, fp, indent=4)
-    return line_names
+    if lines is not None:
+        line_names = { }
+        for line in lines["data"]:
+            line_names[line["id"]] = {
+                "short_name": line["attributes"]["short_name"],
+                "long_name": line["attributes"]["long_name"]
+            }
+        if DEBUG:
+            with open(DEBUG_FOLDER + 'line_names.json', 'w') as fp:
+                json.dump(line_names, fp, indent=4)
+        return line_names
