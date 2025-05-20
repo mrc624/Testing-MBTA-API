93cd14bf5914cee30bd1059ebfb221141814b2c8
Matthew Cloutier <mrcloutier624@gmail.com>
2025-05-19 20:39:12 -0400

    rename and alert helper

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
-import Dictionary_Helper
+import API_Helper
+import Alert_Helper
 
-Dictionary_Helper.Get_ID_to_Names()
-Dictionary_Helper.Get_Vehicles_on_Routes_Count()
-Dictionary_Helper.Get_Lines()
-Dictionary_Helper.Get_Line_Names()
+API_Helper.Get_ID_to_Names()
+API_Helper.Get_Vehicles_on_Routes_Count()
+API_Helper.Get_Lines()
+API_Helper.Get_Line_Names()
+API_Helper.Get_Alerts()
+API_Helper.Get_Facilities()
+Alert_Helper.Update_Current_Effects()
+Alert_Helper.Print_Headers("DELAY")
