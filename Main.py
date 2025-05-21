c230d2cf1839fe5782863441e7bf8a6f5db28f23
Matthew Cloutier <mrcloutier624@gmail.com>
2025-05-20 20:21:33 -0400

    API calls not repeated, each one only called every 15 seconds at most as needed

--- Main.py
+++ Main.py
@@ -1,12 +1,7 @@
 import API_Helper
 import Alert_Helper
 
-API_Helper.Get_ID_to_Names()
-API_Helper.Get_Vehicles_on_Routes_Count()
-API_Helper.Get_Lines()
-API_Helper.Get_Line_Names()
-API_Helper.Get_Alerts()
-API_Helper.Get_Facilities()
+API_Helper.Refresh()
 
 run = True
 
