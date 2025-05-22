167d1142327c0f1ae9a1398515031ba39b6fd358
Matthew Cloutier <mrcloutier624@gmail.com>
2025-05-22 17:06:50 -0400

    started creating options menu

--- Main.py
+++ Main.py
@@ -8,22 +8,19 @@ import Alert_Helper
 import Shape_Helper
 import Map_Helper
 
-#lines = "Green-B,Green-C,Green-D,Green-E,Blue,Red,Orange,Silver"
-lines = "Orange","Red","Blue","Green-B","Green-C","Green-D","Green-E"
-fig, axes = Map_Helper.Draw_Map_With_Stations_Vehicles(lines)
-scheduler = sched.scheduler(time.time, time.sleep)
-scheduler.enter(0, 1, Map_Helper.Continously_Update_Map_With_Vehicles, (scheduler, lines, fig, axes))
-threading.Thread(target=scheduler.run, daemon=True).start()
-
-plt.show()
-
-"""
 run = True
 while run:
-    Alert_Helper.Print_Effect_Options()
-    effect = input("\nInput an Effect\n")
-    if not Alert_Helper.Print_Headers(effect):
-        print("Invalid\n")
-    if effect == "Kill":
+    choice = input("Options:\n1: Live Map\n2: Alerts\n9: Kill\n")
+    if choice == "1" or choice == "Live Map":
+        Map_Helper.update_live_map  = True
+        lines = "Orange","Red","Blue","Green-B","Green-C","Green-D","Green-E"
+        fig, axes = Map_Helper.Draw_Map_With_Stations_Vehicles(lines)
+        scheduler = sched.scheduler(time.time, time.sleep)
+        scheduler.enter(0, 1, Map_Helper.Continously_Update_Map_With_Vehicles, (scheduler, lines, fig, axes))
+        threading.Thread(target=scheduler.run, daemon=True).start()
+        plt.show()
+        Map_Helper.update_live_map = False
+    elif choice == "2" or choice == "Alerts":
+        Alert_Helper.View_Alert_Data()
+    elif choice == "9" or choice == "Kill":
         run = False
-        """
